f380cf3a7f9a368dced309c422345119d26a039d
Santiago-Cortes <santiago.cortes@coxautoinc.com>
2022-05-19 22:45:26 -0500

    Escenarios Santiago

--- cypress/integration/escenarios/aleatorio/escenarios_f001.spec.js
+++ cypress/integration/escenarios/aleatorio/escenarios_f001.spec.js
@@ -3,11 +3,15 @@ const { faker } = require('@faker-js/faker');
 import { AdminDashboard } from "../../pages/admin/dashboard";
 import { Login } from "../../pages/admin/login";
 import { SettingsPage } from "../../pages/admin/settings";
+import { Post } from "../../pages/admin/editor";
+import { Article } from "../../pages/user/article";
 
 import { email as adminEmail, password as adminPassword } from "../../../fixtures/user.json";
 
 const dashboard = new AdminDashboard();
 const settingsPage = new SettingsPage();
+const article = new Article();
+const post = new Post();
 const login = new Login();
 
 
@@ -34,10 +38,56 @@ describe('Funcionalidad F001: Creación de Post', () => {
 
     describe('Escenarios Positivos', () => {
         it('F001E01.EA: ', () => {
+            // GIVEN (additional to the login and dashboard navigation)
+            // that the admin navitages to the dashboard, and selects the option
+            // to create a post, and writes a title and the content for the post
+            post.navigateToEditor();
+            let title = faker.lorem.words();
+            let paragraph = faker.lorem.paragraphs();
+            post.writeTitle(title);
+            post.writeArticle(paragraph);
 
+            // WHEN the admin opens the editor settings menu, and selects the
+            // URL input to erase it an and writes a new url slug
+            let slug = faker.lorem.slug();
+            post.clickEditorSettingsToggle();
+            post.writeUrlSlug(slug);
+            post.clickEditorSettingsToggle();
+            post.publishNow();
+            
+            // THEN after navegating to the post with the new slug,
+            // the title and the content that appears in the article
+            // should match the text that the admin previously wrote
+            article.navigateToArticle(slug);
+            cy.wait(300);
+            article.readTitle((txt) => expect(txt).to.equal(title));
+            article.readContent(prgph => {
+                expect(paragraph).to.contain(prgph);
+            });
         });
         it('F001E03.EA: ', () => {
+            // GIVEN (additional to the login and dashboard navigation)
+            // that the admin navitages to the dashboard, and selects the option
+            // to create a post, and writes a title and the content for the post
+            post.navigateToEditor();
+            let title = faker.lorem.words();
+            let paragraph = faker.lorem.paragraphs();
+            post.writeTitle(title);
+            post.writeArticle(paragraph);
 
+            // WHEN the admin opens the editor settings menu, and selects the
+            // tag input and writes a new tag, and publishes the post
+            let tag = faker.lorem.word();
+            post.clickEditorSettingsToggle();
+            post.setTagPage(tag);
+            post.clickEditorSettingsToggle();
+            post.publishNow();
+
+            // THEN he should be able to open the settings tab, and
+            // the value in the tag input  should match the text that
+            // the admin previously wrote
+            post.clickEditorSettingsToggle();
+            post.readTags((txt) => expect(txt.trim()).to.equal(tag));
         });
         it('F001E05.EA: ', () => {
 
@@ -52,10 +102,57 @@ describe('Funcionalidad F001: Creación de Post', () => {
 
     describe('Escenarios Negativos', () => {
         it('F001E02.EA: ', () => {
+            // GIVEN (additional to the login and dashboard navigation)
+            // that the admin navitages to the dashboard, and selects the option
+            // to create a post, and writes a title and the content for the post
+            post.navigateToEditor();
+            let title = faker.lorem.words();
+            let paragraph = faker.lorem.paragraphs();
+            post.writeTitle(title);
+            post.writeArticle(paragraph);
 
+            // WHEN the admin opens the editor settings menu, and selects the
+            // URL input to erase it an and writes a new url slug, and
+            // publishes the post
+            let slug = faker.lorem.slug(50);
+            post.clickEditorSettingsToggle();
+            post.writeUrlSlug(slug);
+            post.clickEditorSettingsToggle();
+            post.publishNow();
+
+            // THEN after navegating to the post with the new slug,
+            // the title and the content that appears in the article
+            // should match the text that the admin previously wrote
+            article.navigateToArticle(slug);
+            cy.wait(300);
+            article.readTitle((txt) => expect(txt).to.equal(title));
+            article.readContent(prgph => {
+                expect(paragraph).to.contain(prgph);
+            });
         });
         it('F001E04.EA: ', () => {
+            // GIVEN (additional to the login and dashboard navigation)
+            // that the admin navitages to the dashboard, and selects the option
+            // to create a post
+            post.navigateToEditor();
 
+            // WHEN the admin opens the editor settings menu, and selects the
+            // tag input and writes a new tag, and publishes the post
+            let tag = ',' + faker.lorem.word();
+            post.clickEditorSettingsToggle();
+            post.setTagPage(tag);
+            post.clickEditorSettingsToggle();
+
+            // THEN he should be able to write a title and the content
+            // for the post and publish it
+            let title = faker.lorem.words();
+            let paragraph = faker.lorem.paragraphs();
+            post.writeTitle(title);
+            post.writeArticle(paragraph);
+
+            post.publishNow();
+            post.clickEditorSettingsToggle();
+            post.readTags((txt) => expect(txt.trim()).to.equal(tag));
         });
         it('F001E06.EA: ', () => {
 
--- cypress/integration/escenarios/aleatorio/escenarios_f002.spec.js
+++ cypress/integration/escenarios/aleatorio/escenarios_f002.spec.js
@@ -2,12 +2,16 @@ const { faker } = require('@faker-js/faker');
 
 import { AdminDashboard } from "../../pages/admin/dashboard";
 import { Login } from "../../pages/admin/login";
+import { Page } from "../../pages/admin/editor";
+import { Article } from "../../pages/user/article";
 import { SettingsPage } from "../../pages/admin/settings";
 
 import { email as adminEmail, password as adminPassword } from "../../../fixtures/user.json";
 
 const dashboard = new AdminDashboard();
 const settingsPage = new SettingsPage();
+const article = new Article();
+const page = new Page();
 const login = new Login();
 
 
@@ -34,10 +38,56 @@ describe('Funcionalidad F002: Creación de Pages', () => {
 
     describe('Escenarios Positivos', () => {
         it('F002E01.EA: ', () => {
+            // GIVEN (additional to the login and dashboard navigation)
+            // that the admin navitages to the dashboard, and selects the option
+            // to create a page, and writes a title and the content for the page
+            page.navigateToEditor();
+            let title = faker.lorem.words();
+            let paragraph = faker.lorem.paragraphs();
+            page.writeTitle(title);
+            page.writeArticle(paragraph);
 
+            // WHEN the admin opens the editor settings menu, and selects the
+            // URL input to erase it an and writes a new url slug
+            let slug = faker.lorem.slug();
+            page.clickEditorSettingsToggle();
+            page.writeUrlSlug(slug);
+            page.clickEditorSettingsToggle();
+            page.publishNow();
+            
+            // THEN after navegating to the page with the new slug,
+            // the title and the content that appears in the article
+            // should match the text that the admin previously wrote
+            article.navigateToArticle(slug);
+            cy.wait(300);
+            article.readTitle((txt) => expect(txt).to.equal(title));
+            article.readContent(prgph => {
+                expect(paragraph).to.contain(prgph);
+            });
         });
         it('F002E03.EA: ', () => {
+            // GIVEN (additional to the login and dashboard navigation)
+            // that the admin navitages to the dashboard, and selects the option
+            // to create a page, and writes a title and the content for the page
+            page.navigateToEditor();
+            let title = faker.lorem.words();
+            let paragraph = faker.lorem.paragraphs();
+            page.writeTitle(title);
+            page.writeArticle(paragraph);
 
+            // WHEN the admin opens the editor settings menu, and selects the
+            // tag input and writes a new tag, and publishes the page
+            let tag = faker.lorem.word();
+            page.clickEditorSettingsToggle();
+            page.setTagPage(tag);
+            page.clickEditorSettingsToggle();
+            page.publishNow();
+
+            // THEN he should be able to open the settings tab, and
+            // the value in the tag input  should match the text that
+            // the admin previously wrote
+            page.clickEditorSettingsToggle();
+            page.readTags((txt) => expect(txt.trim()).to.equal(tag));
         });
         it('F002E05.EA: ', () => {
 
@@ -52,10 +102,57 @@ describe('Funcionalidad F002: Creación de Pages', () => {
 
     describe('Escenarios Negativos', () => {
         it('F002E02.EA: ', () => {
+            // GIVEN (additional to the login and dashboard navigation)
+            // that the admin navitages to the dashboard, and selects the option
+            // to create a page, and writes a title and the content for the page
+            page.navigateToEditor();
+            let title = faker.lorem.words();
+            let paragraph = faker.lorem.paragraphs();
+            page.writeTitle(title);
+            page.writeArticle(paragraph);
 
+            // WHEN the admin opens the editor settings menu, and selects the
+            // URL input to erase it an and writes a new url slug, and
+            // publishes the page
+            let slug = faker.lorem.slug(50);
+            page.clickEditorSettingsToggle();
+            page.writeUrlSlug(slug);
+            page.clickEditorSettingsToggle();
+            page.publishNow();
+
+            // THEN after navegating to the page with the new slug,
+            // the title and the content that appears in the article
+            // should match the text that the admin previously wrote
+            article.navigateToArticle(slug);
+            cy.wait(300);
+            article.readTitle((txt) => expect(txt).to.equal(title));
+            article.readContent(prgph => {
+                expect(paragraph).to.contain(prgph);
+            });
         });
         it('F002E04.EA: ', () => {
+            // GIVEN (additional to the login and dashboard navigation)
+            // that the admin navitages to the dashboard, and selects the option
+            // to create a page
+            page.navigateToEditor();
 
+            // WHEN the admin opens the editor settings menu, and selects the
+            // tag input and writes a new tag, and publishes the page
+            let tag = ',' + faker.lorem.word();
+            page.clickEditorSettingsToggle();
+            page.setTagPage(tag);
+            page.clickEditorSettingsToggle();
+
+            // THEN he should be able to write a title and the content
+            // for the page and publish it
+            let title = faker.lorem.words();
+            let paragraph = faker.lorem.paragraphs();
+            page.writeTitle(title);
+            page.writeArticle(paragraph);
+
+            page.publishNow();
+            page.clickEditorSettingsToggle();
+            page.readTags((txt) => expect(txt.trim()).to.equal(tag));
         });
         it('F002E06.EA: ', () => {
 
