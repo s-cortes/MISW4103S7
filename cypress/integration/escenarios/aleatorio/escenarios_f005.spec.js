3afa9cfa9ee2081d72c0ac87c6ce554caf6f8f29
afrodriguezm1 <af.rodriguezm1@uniandes.edu.co>
2022-05-21 17:12:22 -0500

    F005 Escenarios 1, 2, 3 y 4

--- cypress/integration/escenarios/aleatorio/escenarios_f005.spec.js
+++ cypress/integration/escenarios/aleatorio/escenarios_f005.spec.js
@@ -1,53 +1,119 @@
-const { faker } = require('@faker-js/faker');
+const { faker } = require("@faker-js/faker");
 
 import { AdminDashboard } from "../../pages/admin/dashboard";
 import { Login } from "../../pages/admin/login";
 import { SettingsPage } from "../../pages/admin/settings";
+import { Profile } from "../../pages/admin/profile";
 
-import { email as adminEmail, password as adminPassword } from "../../../fixtures/user.json";
+import {
+  email as adminEmail,
+  password as adminPassword,
+} from "../../../fixtures/user.json";
 
 const dashboard = new AdminDashboard();
 const settingsPage = new SettingsPage();
 const login = new Login();
-
+const profile = new Profile();
 
 /**
  * Agrupación de Escenarios por Funcionalidad
  * F005: Edición Admin Profile
  */
-describe('Funcionalidad F005: Edición Admin Profile', () => {
+describe("Funcionalidad F005: Edición Admin Profile", () => {
+  before(() => {
+    faker.seed(10005);
+    login.login(adminEmail, adminPassword);
+  });
 
-    before(() => {
-        faker.seed(10005);
-        login.login(adminEmail, adminPassword);
+  after(() => {
+    settingsPage.navigateToLabs();
+    settingsPage.deleteAllContent();
+  });
+
+  beforeEach(() => {
+    Cypress.Cookies.preserveOnce("ghost-admin-api-session");
+    dashboard.navigate();
+  });
+
+  describe("Escenarios Positivos", () => {
+    it("F005E01.EA: ", () => {
+      // GIVEN (additional to the login and dashboard navigation)
+      // that the admin navitages to the dashboard, and goes to
+      // your profile
+      dashboard.navigate();
+      profile.openProfile();
+
+      let name = faker.lorem.words();
+      profile.writeName(name);
+
+      profile.saveProfile();
+
+      cy.wait(300);
+      dashboard.navigate();
+      cy.wait(300);
+
+      profile.openProfile();
+      profile.readName((txt) => expect(txt).to.equal(name));
     });
+    it("F005E03.EA: ", () => {
+      // GIVEN (additional to the login and dashboard navigation)
+      // that the admin navitages to the dashboard, and goes to
+      // your profile
+      dashboard.navigate();
+      profile.openProfile();
 
-    after(() => {
-        settingsPage.navigateToLabs();
-        settingsPage.deleteAllContent();
+      let slug = faker.lorem.slug();
+      profile.writeSlug(slug);
+
+      profile.saveProfile();
+
+      cy.wait(300);
+      dashboard.navigate();
+      cy.wait(300);
+
+      profile.openProfile();
+      profile.readSlug((txt) => expect(txt).to.equal(slug));
     });
+  });
 
-    beforeEach(() => {
-        Cypress.Cookies.preserveOnce('ghost-admin-api-session');
-        dashboard.navigate();
+  describe("Escenarios Negativos", () => {
+    it("F005E02.EA: ", () => {
+      // GIVEN (additional to the login and dashboard navigation)
+      // that the admin navitages to the dashboard, and goes to
+      // your profile
+      dashboard.navigate();
+      profile.openProfile();
+
+      let adminName = faker.lorem.words(192);
+      profile.writeName(adminName);
+
+      profile.saveProfile();
+
+      cy.wait(300);
+      dashboard.navigate();
+      cy.wait(300);
+
+      profile.openProfile();
+      profile.readName((txt) => expect(txt).to.equal(adminName));
     });
+    it("F005E04.EA: ", () => {
+        // GIVEN (additional to the login and dashboard navigation)
+      // that the admin navitages to the dashboard, and goes to
+      // your profile
+      dashboard.navigate();
+      profile.openProfile();
 
-    describe('Escenarios Positivos', () => {
-        it('F005E01.EA: ', () => {
+      let slug = faker.lorem.slug(60);
+      profile.writeSlug(slug);
 
-        });
-        it('F005E03.EA: ', () => {
+      profile.saveProfile();
 
-        });
+      cy.wait(300);
+      dashboard.navigate();
+      cy.wait(300);
+
+      profile.openProfile();
+      profile.readSlug((txt) => expect(txt).to.equal(slug));
     });
-
-    describe('Escenarios Negativos', () => {
-        it('F005E02.EA: ', () => {
-
-        });
-        it('F005E04.EA: ', () => {
-
-        });
-    });
-
+  });
 });
---
+++ cypress/integration/pages/admin/profile.js
@@ -0,0 +1,47 @@
+import { adminUrls } from "../../../fixtures/urls";
+
+export class Profile {
+  openProfile() {
+    cy.get("div.gh-user-avatar").first().click();
+    cy.wait(500);
+    cy.contains("a.dropdown-item", "Your profile").first().click();
+    cy.wait(500);
+    cy.get("div.items-center > div.gh-user-avatar").first().click();
+    cy.wait(500);
+  }
+
+  saveProfile() {
+    cy.get("button.gh-btn-primary").first().click();
+    cy.wait(300);
+  }
+
+  writeName(name) {
+    
+    cy.get('#user-name').clear().type(name);
+    cy.wait(500)
+    
+    return name;
+  }
+
+  writeSlug(name) {
+    
+    cy.get('#user-slug').clear().type(name);
+    cy.wait(500)
+    
+    return name;
+  }
+
+  readName(callback) {
+    cy.get("#user-name")
+      .first()
+      .invoke("val")
+      .then((val) => callback(val));
+  }
+
+  readSlug(callback) {
+    cy.get("#user-slug")
+      .first()
+      .invoke("val")
+      .then((val) => callback(val));
+  }
+}
