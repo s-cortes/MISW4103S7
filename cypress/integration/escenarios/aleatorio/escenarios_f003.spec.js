ab3b960765cc8e96defb4281e9a20fa42fa937b6
cccortesc1 <cc.cortesc1@uniandes.edu.co>
2022-05-22 01:13:11 -0500

    Funcionalidad Tags escenearios aleatorios

--- cypress/integration/escenarios/aleatorio/escenarios_f003.spec.js
+++ cypress/integration/escenarios/aleatorio/escenarios_f003.spec.js
@@ -3,11 +3,13 @@ const { faker } = require('@faker-js/faker');
 import { AdminDashboard } from "../../pages/admin/dashboard";
 import { Login } from "../../pages/admin/login";
 import { SettingsPage } from "../../pages/admin/settings";
+import { Tag } from "../../pages/admin/tag";
 
 import { email as adminEmail, password as adminPassword } from "../../../fixtures/user.json";
 
 const dashboard = new AdminDashboard();
 const settingsPage = new SettingsPage();
+const tag = new Tag();
 const login = new Login();
 
 
@@ -33,9 +35,50 @@ describe('Funcionalidad F003: Creación de Tags', () => {
     });
 
     describe('Escenarios Positivos', () => {
-        it('F003E01.EA: ', () => {
+        it('F003E01.EA: ', () => { 
+            // GIVEN (additional to the login)
 
+            //Create a Tag
+            dashboard.getTags();
+            tag.createNewTag();
+
+            //Get a TagName
+            let name = faker.lorem.word();
+            //Get a TagDescription
+            let description = faker.lorem.paragraph();
+            //Get a TagSlug
+            let slug = faker.lorem.slug();
+            //Get a TagColor
+            let color = Math.floor(Math.random()*16777215).toString(16); //faker.providers.color(); //faker.Color.rgb_hex(); //commerce.color().replace('#', '');
+            console.log('name: ' + name);
+            console.log('description: ' + description);
+            console.log('slug: ' + slug);
+            console.log('color: ' + color);
+
+            //When enter the tag info
+            //Set TagName
+            tag.WriteTagName(name);
+            //Set TagDescription
+            tag.WriteTagColor(color);
+            //Set TagSlug
+            tag.WriteTagSlug(slug);
+            //Set TagColor
+            tag.WriteTagDesc(description);
+
+            //Save the tag
+            tag.saveTag();
+
+            //Then go to the tags list
+            dashboard.getTags();
+
+            //And Confirm the tag name
+            tag.getTagFromListByName(name, (tItem) => {
+                tItem.click();
+                cy.wait(300);
+                tag.ReadTagName((txt) => expect(txt).to.equal(name));
+            })
         });
+
         it('F003E03.EA: ', () => {
 
         });
@@ -49,7 +92,47 @@ describe('Funcionalidad F003: Creación de Tags', () => {
 
     describe('Escenarios Negativos', () => {
         it('F003E02.EA: ', () => {
+            // GIVEN (additional to the login)
 
+            //Create a Tag
+            dashboard.getTags();
+            tag.createNewTag();
+
+            //Get a TagName
+            let name = faker.lorem.word(200);
+            //Get a TagDescription
+            let description = faker.lorem.paragraph();
+            //Get a TagSlug
+            let slug = faker.lorem.slug();
+            //Get a TagColor
+            let color = Math.floor(Math.random()*16777215).toString(16); //faker.providers.color(); //faker.Color.rgb_hex(); //commerce.color().replace('#', '');
+            console.log('name: ' + name);
+            console.log('description: ' + description);
+            console.log('slug: ' + slug);
+            console.log('color: ' + color);
+
+            //When enter the tag info
+            //Set TagName
+            tag.WriteTagName(name);
+            //Set TagDescription
+            tag.WriteTagColor(color);
+            //Set TagSlug
+            tag.WriteTagSlug(slug);
+            //Set TagColor
+            tag.WriteTagDesc(description);
+
+            //Save the tag
+            tag.saveTag();
+
+            //Then go to the tags list
+            dashboard.getTags();
+
+            //And Confirm the tag name
+            tag.getTagFromListByName(name, (tItem) => {
+                tItem.click();
+                cy.wait(300);
+                tag.ReadTagName((txt) => expect(txt).to.equal(name));
+            })
         });
         it('F003E04.EA: ', () => {
 
--- cypress/integration/pages/admin/tag.js
+++ cypress/integration/pages/admin/tag.js
@@ -13,22 +13,36 @@ export class Tag {
         cy.get('#tag-name').type(name);
         cy.wait(300);
         return name;
-    }
-    ReadTagName(callback) {
-        cy.get('#tag-name').val(val => callback(val));
-    }
+    }    
     WriteTagDesc(desc) {
         cy.get('#tag-description').type(desc)
             .invoke('text', desc);
         cy.wait(300);
-
         return desc;
+    }
+    WriteTagSlug(slug) {
+        cy.get('input[name="slug"]').type(slug);
+        cy.wait(300);
+        return slug;
+    }
+    WriteTagColor(color) {
+        cy.get('div.input-color > input').type(color);
+        cy.wait(300);
+        return color;
+    }
+    ReadTagName(callback) {
+        cy.get('div.gh-tag-settings-multiprop > div > input').invoke('val')
+        .then(val => callback(val));
     }
     readTagDesc(callback) {
         cy.get('#tag-description').invoke('text').then(val => callback(val));
     }
+    getTagFromListByName(name, callback) {
+        let tagItem = cy.contains('li', name).first();
+        callback(tagItem);
+    }
     saveTag() {
-        cy.get('button.ember-view[class="gh-btn gh-btn-primary gh-btn-icon ember-view"]').click();
+        cy.get('button[class="gh-btn gh-btn-primary gh-btn-icon ember-view"]').click();
         cy.wait(300);
     }
 }
